56bf3d371c3ac6b94660cf5617b5d4b1e736078b
watpekin <henrique.watpequim@gmail.com>
2020-12-04 09:46:04 -0300

    Part 1 Relacionamento do banco de dados

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	/**
 	 * 
@@ -20,6 +25,11 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
 	public User() {
 		
 	}
@@ -61,7 +71,9 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+	public List<Order> getOrder(){
+		return orders;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
