5d34e4625dd6af4f0b849553a3f6954e84e701e8
watpekin <henrique.watpequim@gmail.com>
2020-12-05 10:42:39 -0300

    Subtotal & Total Methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -94,6 +94,13 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -101,7 +108,7 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
