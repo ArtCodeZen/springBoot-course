8cbb71fb7b4131b30002da38715651c8dd67fe74
watpekin <henrique.watpequim@gmail.com>
2020-12-05 10:32:36 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.Payment;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.enums.OrderStatus;
@@ -65,6 +66,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi2 = new OrderItem(o1, p2, 10, p1.getPrice());
 		
 		OrderItemRepository.saveAll(Arrays.asList(oi1, oi2));
+		
+		// pagamentos
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		OrderRepository.save(o1);
 		
 		
 	}
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.course.enums.OrderStatus;
@@ -35,6 +37,8 @@ public class Order implements Serializable {
 	
 	private Integer orderStatus;
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	@JsonIgnore
 	@ManyToOne
@@ -113,6 +117,14 @@ public class Order implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	
 	
